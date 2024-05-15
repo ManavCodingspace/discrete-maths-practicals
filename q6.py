37f47941a739dafcfebee73209c16e7dffa209c4
ManavCodingSpace <145857624+ManavCodingspace@users.noreply.github.com>
2024-05-15 23:37:05 +0530

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,31 +1,36 @@
 class Graph:
-    def __init__(self, num_vertices, adjacency_matrix):
-        self.num_vertices = num_vertices
-        self.adjacency_matrix = adjacency_matrix
-
-    def is_complete_graph(self):
-        for i in range(self.num_vertices):
-            for j in range(self.num_vertices):
-                if i != j and self.adjacency_matrix[i][j] == 0:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.adj_matrix = [[0] * vertices for _ in range(vertices)]
+        
+    def add_edge(self, u, v):
+        if graph_type == 1:
+            self.adj_matrix[u][v] = 1
+            self.adj_matrix[v][u] = 1
+        else:1
+        self.adj_matrix[u][v] = 1
+            
+    def is_complete(self):
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                if i != j and self.adj_matrix[i][j] == 0:
                     return False
         return True
-
-def main():
-    # Example adjacency matrix representing a graph
-    adjacency_matrix = [
-        [0, 1, 1, 1],
-        [1, 0, 1, 1],
-        [1, 1, 0, 1],
-        [1, 1, 1, 0]
-    ]
-    num_vertices = 4  # Number of vertices in the graph
-
-    graph = Graph(num_vertices, adjacency_matrix)
-
-    if graph.is_complete_graph():
-        print("The given graph is a complete graph.")
-    else:
-        print("The given graph is not a complete graph.")
+    
+    def get_matrix(self):
+        return self.adj_matrix
 
 if __name__ == "__main__":
-    main()
+    graph_type = int(input("Enter Your Graph Type(1.Undirected 2.Directed):: "))
+    num_vertices = int(input("Enter number of vertices:: "))
+    g = Graph(num_vertices)
+    num = int(input("Enter number of edges:: "))
+    for i in range(num):
+        a = int(input(f"Enter first vertice of {i+1} edge:: ")) - 1
+        b = int(input(f"Enter second vertice of same edge:: ")) - 1
+        g.add_edge(a, b)
+    print(g.get_matrix())
+    if g.is_complete():
+        print("The graph is a complete graph.")
+    else:
+        print("The graph is not a complete graph.")
