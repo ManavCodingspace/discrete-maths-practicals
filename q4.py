24843526571ecdbd778f3105f81017466e35f0c2
ManavCodingSpace <145857624+ManavCodingspace@users.noreply.github.com>
2024-05-15 23:12:41 +0530

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,23 +1,11 @@
-def find_solutions(n, C):
-    solutions = []
-    for x1 in range(C + 1):
-        for x2 in range(C + 1):
-            for x3 in range(C + 1):
-                # Continue this pattern for n variables
-                # To generalize this for n, you can use itertools.product
-                if x1 + x2 + x3 == C:
-                    solutions.append((x1, x2, x3))  # Add the solution to the list
+from itertools import product
+
+def find_solutions(n,c):
+    solutions = [p for p in product(range (c+1), repeat = n) if sum(p) == c]
     return solutions
 
-def main():
-    n = 3  # Number of variables
-    C = 5  # Constant
-
-    solutions = find_solutions(n, C)
-
-    print(f"Solutions for x1 + x2 + x3 = {C}:")
-    for sol in solutions:
-        print(sol)
-
-if __name__ == "__main__":
-    main()
+n = 3
+c = 5
+solutions = find_solutions(n,c)
+for solution in solutions:
+    print(solution)     
